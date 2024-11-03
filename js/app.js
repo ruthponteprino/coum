0226f14c6dbb0b5564292a1eebb3bc0450d19b8f
ruthponteprino <rponteprino@gmail.com>
2024-11-03 13:37:19 -0300

    Agregue nav responsive, falta arreglar estilos

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const btnMobile = document.querySelector('#boton-mobile')
+const btnMenu = document.querySelector('#menu-mobile')
+const menuConSub = document.querySelectorAll('.menu-con-sub')
+const subM = document.querySelectorAll('.submenu')
+
+btnMobile.addEventListener('click', () => {
+    btnMenu.classList.toggle('hidden')
+})
+
+// Agregamos un listener a cada elemento del menú que tiene submenús
+menuConSub.forEach((menu, index) => {
+    menu.addEventListener('click', () => {
+        // Mostramos/ocultamos el submenú correspondiente
+        subM[index].classList.toggle('hidden'); // Usamos el índice para afectar al submenú correcto
+    });
+});
+
+
+
+const btnDesktop = document.querySelectorAll('.boton-desktop')
+const subDesktop = document.querySelectorAll('.submenu-desktop')
+
+btnDesktop.forEach((menu, index) => {
+    menu.addEventListener('click', () => {
+        subDesktop[index].classList.toggle('hidden');
+    });
+});
